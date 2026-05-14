8ce159b1c711813254b7ba8d676d01c8ebf990d2
impossiburu <54071319+impossiburu@users.noreply.github.com>
2026-05-14 11:59:14 +0300

    Create Constants.cs

---
+++ SDAWorshipDraft/Helpers/Constants.cs
@@ -0,0 +1,8 @@
+namespace SDAWorshipDraft.Helpers;
+
+public static class Constants
+{
+    public const string StructFile = "struct.json";
+    public const string ConfigFile = ".config/config.json";
+    public const string HymnsFile = "data/hymns.json";
+}
